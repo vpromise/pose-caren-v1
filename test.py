a518af05f6bd80b3a0ca49cde8682acd11849325
vpromise <vpromisever@gmail.com>
2019-11-07 16:12:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,14 +6,10 @@ from network import *
 params = Tester.TestParams()
 params.gpus = [0]  # set 'params.gpus=[]' to use CPU model. if len(params.gpus)>1, default to use params.gpus[0] to test
 params.ckpt = './models/ckpt_epoch_300.pth' 
-params.testdata_dir = './dataset/_64/test.txt'
+params.testdata_dir = './dataset/128/test.txt'
 
 # models
 model = StackedHourGlass(256, 1, 2, 4, 3)
-# model = resnet34(pretrained=False, num_classes=1000)  # batch_size=120, 1GPU Memory < 7000M
-# model.fc = nn.Linear(512, 6)
-# model = resnet101(pretrained=False,num_classes=1000)  # batch_size=60, 1GPU Memory > 9000M
-# model.fc = nn.Linear(512*4, 6)
 
 # Test
 tester = Tester(model, params)
